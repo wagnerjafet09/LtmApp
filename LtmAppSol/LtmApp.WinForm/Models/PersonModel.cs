237c19daba3ff13635a547031d390b2aae97632a
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2023-02-11 04:25:55 -0400

    Clase persona completa

--- LtmAppSol/LtmApp.WinForm/Models/PersonModel.cs
+++ LtmAppSol/LtmApp.WinForm/Models/PersonModel.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LtmApp.WinForm.Exceptions;
 
 namespace LtmApp.WinForm.Models
 {
-    internal class Person
+    public class PersonModel
     {
+        private string firstName;
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new StudentException("El campo nombre es requerido");
+                }
+                else if (value.Length > 50)
+                {
+                    throw new StudentException("La longitud del nombre es invalida");
+                }
+            }
+        }
+        private string lastName;
+        public string LastName
+        {
+            get { return this.lastName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new StudentException("El campo nombre es requerido");
+                }
+                else if (value.Length > 50)
+                {
+                    throw new StudentException("La longitud del nombre es invalida");
+                }
+            }
+        }
     }
 }
