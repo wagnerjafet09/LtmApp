f8730602641c5f29bc165629267385f02f9a3adf
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2023-02-12 02:11:37 -0400

    Creacion de InstructorModel

---
+++ LtmAppSol/LtmApp.WinForm/Models/InstructorModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using LtmApp.WinForm.Exceptions;
+
+namespace LtmApp.WinForm.Models
+{
+    public class InstructorModel : PersonModel
+    {
+        private int instructorId;
+        public int InstructorId
+        {
+            get { return this.instructorId; }
+            set
+            {
+                if ((value) == null)
+                {
+                    throw new StudentException("El campo Id es requerido");
+                }
+            }
+        }
+
+        public DateTime HireDate { get; set; }
+    }
+}
