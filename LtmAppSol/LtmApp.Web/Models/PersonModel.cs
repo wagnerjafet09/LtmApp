0ba27a165501ec7b99d2d2ed387c435cc4c9dfba
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2023-02-13 12:19:19 -0400

    StudentModel y PersonModel con validaciones

---
+++ LtmAppSol/LtmApp.Web/Models/PersonModel.cs
@@ -0,0 +1,50 @@
+﻿using LtmApp.Web.Exceptions;
+
+namespace LtmApp.Web.Models
+{
+    public class PersonModel
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new StudentException("El campo apellido es requerido");
+                }
+                else if (value.Length > 50)
+                {
+                    throw new StudentException("La longitud del apellido es inválida");
+                }
+                else
+                {
+                    firstName = value;
+                }
+            }
+        }
+
+
+        private string lastName;
+        public string LastName 
+        {
+            get { return lastName; }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new StudentException("El campo apellido es requerido");
+                }
+                else if (value.Length > 50) 
+                {
+                    throw new StudentException("La longitud del apellido es inválida");
+                }
+                else
+                {
+                    lastName = value;
+                }
+            } 
+        }
+    }
+}
