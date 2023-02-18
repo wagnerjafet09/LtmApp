ce2b5ff52e4cc497aeac912cd3b0a0e3d7a7e22d
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2023-02-17 20:47:07 -0400

    Carpeta Models, entity Person y BaseEntity

---
+++ LtmAppSol/LtmApp.DAL/Core/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LtmApp.DAL.Core
+{
+    public abstract class Person : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
